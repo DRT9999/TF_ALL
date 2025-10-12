2e7a4b9d9c457b3e64237d5a393cb76dd88d8d03
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-12 18:45:15 +0530

    module

--- expense4/app/main.tf
+++ expense4/app/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Expenes" {
     #for_each = var.comp
     ami = var.ami  #"ami-0fcc78c828f981df2"
     instance_type = var.instance_type #try (each.value["instance_type"], null) == ".*" ? each.value["instance_type"] : "t2.small"
-    vpc_security_group_ids = var.vpc_security_group_ids  #["sg-052508cac91923258"] 
+    vpc_security_group_ids = [var.vpc_security_group_ids]  #["sg-052508cac91923258"] 
 
     tags = {
         Name = var.name
--- expense4/main.tf
+++ expense4/main.tf
@@ -3,7 +3,7 @@ module "App" {
     source = "./app"
     ami = var.ami                               #"ami-0fcc78c828f981df2"
     instance_type = each.value["instance_type"] #try (each.value["instance_type"], null) == ".*" ? each.value["instance_type"] : "t2.small"
-    vpc_security_group_ids = var.vpc_security_group_ids
+    vpc_security_group_ids = [var.vpc_security_group_ids]
     name = each.key   
 }
 
