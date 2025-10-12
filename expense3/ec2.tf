486671c72ff9710a181a1d390940d18305654a2c
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-12 11:28:29 +0530

    expense2

---
+++ expense3/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "Expenes" {
+    #count = length(var.comp)
+    for_each = var.comp
+    ami = "ami-0fcc78c828f981df2"
+    instance_type = each.value["instance_type"] == ".*" ? each.value["instance_type"] : "t2.micro"
+    vpc_security_group_ids = ["sg-052508cac91923258"] 
+
+    tags = {
+
+        Name = each.key
+        bu = each.value["bu"]
+    }
+}
+
+variable "comp" {
+
+    default = {
+        frontend = {                                # each.key
+            instance_type =  "t2.micro"
+            bu = "APP"
+        }
+        mysql ={
+            instance_type =  "t2.micro"             # each.value
+            bu = "DB"
+        }
+        backend ={
+            #instance_type =  "t2.micro"
+        }
+  
+    }   
+}
