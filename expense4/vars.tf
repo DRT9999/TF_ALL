77bd6657845bb6105d8f796220099f96b9c0aa16
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-13 15:49:13 +0530

    provision

--- expense4/vars.tf
+++ expense4/vars.tf
@@ -1,13 +1,13 @@
 variable "comp" {
     default = {
-        frontend = {                                # each.key
-            instance_type =  "t2.micro" 
-        }
         mysql ={
             instance_type =  "t2.micro"             # each.value
         }
         backend ={
             instance_type =  "t2.micro"
+        }
+        frontend = {                                # each.key
+            instance_type =  "t2.micro" 
         }
     }   
 }
