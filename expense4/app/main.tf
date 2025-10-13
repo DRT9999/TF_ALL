0e67624a85025143be8eafb18038cb89af3a8c97
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-13 20:05:21 +0530

    provision

--- expense4/app/main.tf
+++ expense4/app/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "Expenes" {
 
 resource "aws_route53_record" "DNS" {
   zone_id = var.zone_id
-  name    = "${var.name}-qa.exp.in"
+  name    = "${var.name}-dev.exp.in"
   type    = "A"
   ttl     = 10
   records = [aws_instance.Expenes.private_ip]
